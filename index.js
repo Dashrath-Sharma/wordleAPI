792c109aaa0d1e7d5294659124a2db5a66cc44cd
Dashrath-Sharma <dashrathsharma7838@gmail.com>
2023-02-12 13:13:20 +0530

    5 letter words API

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const words = require('./words.json')
+
+//Setting PORT Assigned by the Client System or 3000 in case there isn't any
+const port = process.env.PORT || 3000;
+
+//Sending Array of 5 Letter words
+app.get('/', (req, res) => {
+    res.send(words)
+});
+
+app.listen(port, console.log(`Server running on ${port}`))
