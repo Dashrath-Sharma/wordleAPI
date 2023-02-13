86ccd96122415668ca0ba04b2b2b6f17f4c10834
Dashrath-Sharma <dashrathsharma7838@gmail.com>
2023-02-14 00:42:13 +0530

    Updated Cors

--- functions/api.js
+++ functions/api.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const cors = require('cors')
 const words = require('./words.json')
 const app = express();
-
+app.use(cors())
 const router = express.Router();
 
 router.get('/', (req, res) => {
